2307b1b8560b046b55f2f0764a6108adabbbe2db
davedev <davidabolade29@gmail.com>
2023-01-22 06:44:43 +0100

    backkground color fixed

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <main className='bg-primary flex justify-between items-center px-[100px] py-[20px]'>
+    <main className=' flex justify-between items-center px-[100px] py-[20px] my-[100px]'>
       <div className='text-white font-lobster text-[75px] leading-[90px] font-n'>
         <p>Hi,</p>
         <p>I am <span className='font-pacifico font-vb text-secondary text-[100px] leading-[120px]'>d</span>avid,</p>
         <p>Web Developer</p>
         <p className='text-[16px] leading-[24px] text-[#cccccc] font-poppins mt-[20px] mb-[10px]'>Frontend Developer, Profficient in React & Node</p>
-        <button className='text-[18px] leading-[22px] font-inter font-vb bg-secondary px-[50px] py-[20px] rounded-[5px]'><Link to='/projects'>My Projects</Link></button>
+        <button className='text-[18px] leading-[22px] font-inter font-vb bg-secondary px-[50px] py-[20px] rounded-[5px]'><Link to='/projects' className='bg-secondary'>My Projects</Link></button>
       </div>
       <div>
         <img src={heroImg} alt="hero_img" className='w-[498px] h-[388px]'/>
