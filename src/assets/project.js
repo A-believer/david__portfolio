a190844711ebd14ac844d364f5f0f96522fe977e
davedev <davidabolade29@gmail.com>
2023-01-27 22:45:34 +0100

    update: Project section setup complete

---
+++ src/assets/project.js
@@ -0,0 +1,117 @@
+import pro1 from "./img1.png"
+import pro2 from "./img2.png"
+import pro3 from "./img3.png"
+import pro4 from "./img4.png"
+import pro5 from "./img5.png"
+import pro6 from "./img6.png"
+import pro7 from "./img7.png"
+import pro8 from "./img8.png"
+import pro9 from "./img9.png"
+
+const proData = [
+    {
+    id: 1,
+    title: "Random Quotes App",
+    img: pro1,
+    link: "https://random-quotes-app-pi.vercel.app/",
+    tags: {
+        1: "HTML",
+        2: "CSS",
+        3: "Javascript",
+        4: "React"
+    },
+    about: "This website gives random advice at the click of a button. The advices are gotten from an api."
+    },
+{
+    id: 2,
+    title: "Hoobank App",
+    img: pro2,
+    link: "https://my-modern-bank-app.vercel.app/",
+    tags: {
+        1: "HTML",
+        2: "CSS",
+        3: "Javascript",
+        4: "Vite",
+        5: "React",
+        6: "Tailwindcss"
+    },
+    about: "A fully responsive modern bank landing page. Featuring gradients and beautiful tailwind effects"
+    },
+{
+    id: 3,
+    title: "Netflix Clone",
+    img: pro3,
+    link: "https://my-netflix-clone-nine.vercel.app/",
+    tags: {
+        1: "HTML",
+        2: "Tailwindcss"
+    },
+    about: "A simple clone of the netflix landing page built from scratch."
+    },
+{
+    id: 4,
+    title: "GPT-3",
+    img: pro4,
+    link: "https://gpt3-app-one.vercel.app/",
+    tags: {
+        1: "HTML",
+        2: "CSS",
+        3: "BEM",
+        4: "React",
+    },
+    about: "A template web-app highlighting the uses of Open AI."
+    },
+{
+    id: 5,
+    title: "Gericht Restaurant App",
+    img: pro5,
+    link: "https://my-gericht-restaurant.netlify.app/",
+    tags: {
+        1: "HTML",
+        2: "CSS",
+        3: "Javascript",
+        4: "React"
+    },
+    about: "A simple but restaurant website with section for food and drinks. "
+    },
+{
+    id: 6,
+    title: "Team Allocation App",
+    img: pro6,
+    link: "https://team-member-alloaction-app.vercel.app/",
+    tags: {
+        1: "HTML",
+        2: "CSS",
+        3: "Javascript",
+        4: "React"
+    },
+    about: "This website illustrates how to group a company's employees into different teams efficiently."
+    },
+{
+    id: 7,
+    title: "Meals App",
+    img: pro7,
+    link: "https://recipe-app-seven-psi.vercel.app/",
+    tags: {
+        1: "HTML",
+        2: "CSS",
+        3: "Javascript",
+        4: "React"
+    },
+    about: "This webapp show different meals and how they are made. If you are sure on which meal click the 'suprise me!' button for a random meal. The meals are fetched from an api."
+    },
+{
+    id: 8,
+    title: "Interactive Rating Component",
+    img: pro8,
+    link: "http://fm-interactive-rating-component-one.vercel.app/",
+    tags: {
+        1: "HTML",
+        2: "CSS",
+        3: "Javascript"
+    },
+    about: "This is my submission for the frontend mentor challenge. Simple but interactive"
+    },
+]
+
+export default proData;
